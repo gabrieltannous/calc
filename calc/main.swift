2e759bad025ab7d76a2c46b43ad97522536b1c36
Gabriel Tannous <gabriel@Gabriels-Mac.local>
2019-03-27 00:21:41 +1100

    All tests passes (Functionality 100%) - Remains Structure

--- calc/main.swift
+++ calc/main.swift
@@ -11,4 +11,89 @@ import Foundation
 var args = ProcessInfo.processInfo.arguments
 args.removeFirst() // remove the name of the program
 
-print(Int(args[0])!)
+func printArgs() {
+    for i in args {
+        print(i, terminator: " ")
+    }
+    print("")
+}
+
+let acceptableCharacters = ["x", "/", "%", "(", ")", "+", "-"]
+
+for i in args {
+    if Int(i) == nil {
+        if !acceptableCharacters.contains(i) {
+            print("You have entered an invalid character")
+            exit(1)
+        }
+    }
+}
+
+for i in 0..<args.count - 1 {
+    let num1 = Int(args[i])
+    if num1 != nil {
+        let num2 = Int(args[i+1])
+        if num2 != nil {
+            exit(1)
+        }
+    }
+}
+
+for i in 0..<args.count {
+    if args[i] == "x" || args[i] == "/" || args[i] == "%" {
+        if args[i+1] == "x" || args[i+1] == "/" || args[i+1] == "%" {
+            exit(1)
+        }
+        let prev = Int(args[i-1])
+        let next = Int(args[i+1])
+        var total = prev
+        switch args[i] {
+            case "x":
+                if let prev = prev, let next = next {
+                    total = prev * next
+                }
+            case "/":
+                if let prev = prev, let next = next {
+                    total = prev / next
+                }
+            case "%":
+                if let prev = prev, let next = next {
+                    total = prev % next
+                }
+            default:
+                exit(1);
+        }
+        if let total = total {
+            args[i+1] = String(total)
+            args[i] = "+"
+            args[i-1] = "0"
+        }
+    }
+}
+
+for i in 0..<args.count {
+    if args[i] == "+" || args[i] == "-" {
+        let prev = Int(args[i-1])
+        let next = Int(args[i+1])
+        var total = prev
+        switch args[i] {
+        case "+":
+            if let prev = prev, let next = next {
+                total = prev + next
+            }
+        case "-":
+            if let prev = prev, let next = next {
+                total = prev - next
+            }
+        default:
+            exit(1);
+        }
+        if let total = total {
+            args[i+1] = String(total)
+            args[i] = "+"
+            args[i-1] = "0"
+        }
+    }
+}
+
+print(Int(args[args.count-1])!)
